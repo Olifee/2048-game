7a1dfc9411c2653a8124a5106646148c9306a822
Olifee <30570207+Olifee@users.noreply.github.com>
2017-07-30 22:04:52 +0200

    Update Tile.java

--- scr/gamelogic/Tile.java
+++ scr/gamelogic/Tile.java
@@ -2,15 +2,9 @@ package gamelogic;
 
 public class TileExistsException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	public TileExistsException() {
 	}
 
-	/**
-	 * @param message
-	 */
 	public TileExistsException(String message) {
 		super(message);
 	}
